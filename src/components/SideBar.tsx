7454ab635ea1608665dbc2032beaaca4bf7f0cea
gustavoabell <gustavoabel.contato@gmail.com>
2021-05-02 14:50:44 -0300

    separe components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  handleClick: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  handleClick,
+  genres,
+  selectedGenreId
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
